7ca20f3e64986b70f08ec8deab18cd3bc2ff1ea9
a895412 <allassaneaymane@gmail.com>
2023-01-31 10:38:03 +0100

    implementing the solution found on:https://www.baeldung.com/jpa-many-to-many with ColdBox class acting like an intermediary between users and student

--- src/main/java/com/dovahkir/foodapp/coldbox/ColdBoxService.java
+++ src/main/java/com/dovahkir/foodapp/coldbox/ColdBoxService.java
@@ -30,40 +30,40 @@ public class ColdBoxService {
     }
 
     // REMOVE THE ResponseEntity return type in the service class. Not  good practice.
-    void addFoodItemToColdBox(Long coldBoxId, Long foodItemId){
-        Optional<ColdBox> coldBox = coldBoxRepo.findById(coldBoxId);
-        Optional<FoodItem> foodItem = foodItemRepo.findById(foodItemId);
-        if(coldBox.isPresent() && foodItem.isPresent()) {
-            coldBox.get().getColdBoxContent().add(foodItem.get());
-            coldBoxRepo.save(coldBox.get());
-        }
-    }
-
-    Optional<List<FoodItem>> getFoodItemsInColdBox(Long coldBoxId){
-        Optional<ColdBox> coldBox = coldBoxRepo.findById(coldBoxId);
-        return coldBox.map(ColdBox::getColdBoxContent);
-    }
-
-    List<ColdBox> getAllColdBox(){
-        return (List<ColdBox>) coldBoxRepo.findAll();
-    }
-
-
-    void changeFoodItemExpiryDate(Long coldBoxId, Long foodItemId, String newDate){
-        Optional<ColdBox> coldBox = coldBoxRepo.findById(coldBoxId);
-        FoodItem foodItemToBeModified = coldBox.get().getColdBoxContent().stream().filter(foodItem -> foodItemId==foodItem.getId()).findAny().orElseThrow(() -> new FoodItemNotFoundException("There doesn't appear to be a food item by that ID"));
-        foodItemToBeModified.setExpiryTime(LocalDateTime.parse(newDate));
-    }
-
-    void deleteFoodItemFromColdBox(Long coldBoxId, Long foodItemId){
-        Optional<ColdBox> coldBox = coldBoxRepo.findById(coldBoxId);
-
-        //When I use the Optional to check whether or not the FoodItem is present. Im checking if it is present in the global database, not if its present in the current coldBox. I dont know if it makes more sense to check inside coldBox
-        Optional<FoodItem> foodItemToBeRemoved = foodItemRepo.findById(foodItemId);
-
-        if(coldBox.isPresent() && foodItemToBeRemoved.isPresent()){
-            coldBox.get().removeFoodItem(foodItemToBeRemoved.get());
-            coldBoxRepo.save(coldBox.get());
-        }
-    }
+//    void addFoodItemToColdBox(Long coldBoxId, Long foodItemId){
+//        Optional<ColdBox> coldBox = coldBoxRepo.findById(coldBoxId);
+//        Optional<FoodItem> foodItem = foodItemRepo.findById(foodItemId);
+//        if(coldBox.isPresent() && foodItem.isPresent()) {
+//            coldBox.get().getColdBoxContent().add(foodItem.get());
+//            coldBoxRepo.save(coldBox.get());
+//        }
+//    }
+//
+//    Optional<List<FoodItem>> getFoodItemsInColdBox(Long coldBoxId){
+//        Optional<ColdBox> coldBox = coldBoxRepo.findById(coldBoxId);
+//        return coldBox.map(ColdBox::getColdBoxContent);
+//    }
+//
+//    List<ColdBox> getAllColdBox(){
+//        return (List<ColdBox>) coldBoxRepo.findAll();
+//    }
+//
+//
+//    void changeFoodItemExpiryDate(Long coldBoxId, Long foodItemId, String newDate){
+//        Optional<ColdBox> coldBox = coldBoxRepo.findById(coldBoxId);
+//        FoodItem foodItemToBeModified = coldBox.get().getColdBoxContent().stream().filter(foodItem -> foodItemId==foodItem.getId()).findAny().orElseThrow(() -> new FoodItemNotFoundException("There doesn't appear to be a food item by that ID"));
+//        foodItemToBeModified.setExpiryTime(LocalDateTime.parse(newDate));
+//    }
+//
+//    void deleteFoodItemFromColdBox(Long coldBoxId, Long foodItemId){
+//        Optional<ColdBox> coldBox = coldBoxRepo.findById(coldBoxId);
+//
+//        //When I use the Optional to check whether or not the FoodItem is present. Im checking if it is present in the global database, not if its present in the current coldBox. I dont know if it makes more sense to check inside coldBox
+//        Optional<FoodItem> foodItemToBeRemoved = foodItemRepo.findById(foodItemId);
+//
+//        if(coldBox.isPresent() && foodItemToBeRemoved.isPresent()){
+//            coldBox.get().removeFoodItem(foodItemToBeRemoved.get());
+//            coldBoxRepo.save(coldBox.get());
+//        }
+//    }
 }
